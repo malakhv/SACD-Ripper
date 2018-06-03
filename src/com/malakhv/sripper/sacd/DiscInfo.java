dcf7b55662b339d7d692e717c791ff06fab4bfbe
malakhv <malakhv@gmail.com>
2018-06-03 23:18:18 +0300

    Added base classes that describe SACD

---
+++ src/com/malakhv/sripper/sacd/DiscInfo.java
@@ -0,0 +1,42 @@
+/* *
+ * Copyright (C) 2013 Mikhail Malakhov <malakhv@live.ru>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+package com.malakhv.sripper.sacd;
+
+/**
+ * The class describes SACD disc information.
+ * @author Mikhail.Malakhov
+ * */
+public final class DiscInfo extends BaseInfo {
+
+
+    /**  */
+    public String date;
+
+
+    public String version;
+
+    @Override
+    public void clear() {
+        super.clear();
+        date = version = null;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscInfo {" + super.toString() + ", version=" + version + ", date=" + date + "}";
+    }
+}
