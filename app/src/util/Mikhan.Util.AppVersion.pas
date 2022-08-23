107d37e7baea00940421b9b7151ef63a31cd3b09
Mikhail Malakhov <malakhv@gmail.com>
2022-08-23 20:37:40 +0300

    Added comments

--- app/src/util/Mikhan.Util.AppVersion.pas
+++ app/src/util/Mikhan.Util.AppVersion.pas
@@ -26,6 +26,46 @@
 {----------------------------------------------------------------}
 { This unit uses "Semantic Versioning 2.0.0" conception, for     }
 { more information about it, please see http://semver.org/.      }
+{                                                                }
+{ The version name represents in format:                         }
+{   XX.YY.ZZZ[.DEBUG][-BRANCH_NAME][-COMMIT_ID], where:          }
+{                                                                }
+{   - XX - major version (max value is 99)                       }
+{   - YY - minor version (max value is 99)                       }
+{   - ZZZ - number of patch (max value is 999)                   }
+{   - DEBUG - the debug flag (only for debug build), may be null }
+{   - BRANCH_NAME - the git branch name, for example "master"    }
+{   - COMMIT_ID - the first seven letters of git commit id,      }
+{     for example "94604e3"                                      }
+{                                                                }
+{ Below, you could see several examples:                         }
+{                                                                }
+{   - 0.1.0.debug-master - initial (default) version for all new }
+{     project (without any commits and on default branch)        }
+{                                                                }
+{   - 1.2.15.debug-master-94604e3 - debug build of version       }
+{     1.2.15, where latest commit id is 94604e3                  }
+{                                                                }
+{   - 1.3.0.master-94604e3 - release build of version 1.3.0      }
+{     where latest commit id is 94604e3                          }
+{                                                                }
+{ The initial (default) value of version is:                     }
+{                                                                }
+{   0.1.0.debug-master                                           }
+{                                                                }
+{ The version code calculate from version name by                }
+{ following rule:                                                }
+{                                                                }
+{   XX * 100000 + YY * 1000 + ZZZ                                }
+{                                                                }
+{ Several examples:                                              }
+{                                                                }
+{   - Name 0.1.0.debug-master-0000000 = Code 1000                }
+{   - Name 0.1.8.debug-develop-0000000 = Code 1008               }
+{   - Name 2.5.17.master-94604e3 = Code 205017                   }
+{   - Name 99.88.777.debug-qa-0000000 = Code 9988777             }
+{                                                                }
+{ The program version can be stored in separate file.            }
 {----------------------------------------------------------------}
 
 unit Mikhan.Util.AppParams;
@@ -37,20 +77,7 @@ Interface
 
 const
 
-    ARG_LIST = '--list';
-    ARG_LIST_SHORT = '-l';
-    ARG_STATUS = '--status';
-    ARG_STATUS_SHORT = '-s';
-    ARG_INFO = '--info';
-    ARG_INFO_SHORT = '-i'
-    ARG_FILE = '--file';
-    ARG_FILE_SHORT = '-f';
 
-    ARG_HELP = '--help';
-    ARG_HELP_SHORT = '-h';
-
-    ARG_VERSION = '--version';
-    ARG_VERSION_SHORT = '-v';
 
 type
 
