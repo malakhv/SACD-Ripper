91c39fdee58225404b37a3111cc9c3f7ed129729
Mikhail Malakhov <malakhv@gmail.com>
2023-08-02 00:06:58 +0300

    Small refactoring build.bat

--- app/build.bat
+++ app/build.bat
@@ -4,14 +4,15 @@
 :: Author: Mikhail.Malakhov
 ::
 
-IF EXIST build\ (rd /s /q build\) ELSE (echo The build directory not found.)
+IF EXIST build\ (rd /s /q build\) ELSE (echo The build directory not found...)
 
 mkdir build
 
 SET PASCAL_KIT="./../../PascalKit/src/util"
 
 fpc.bat ./src/App.pas -FEbuild ^
-    -Fu./src/app -Fu./src/util ^
+    -Fu./src/app ^
+    -Fu./src/util ^
     -Fu./src/rainbow ^
     -Fu%PASCAL_KIT% ^
     -osacd.exe
