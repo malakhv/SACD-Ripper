d53823bcfcc1636389a70efdde53217794461af6
Mikhail Malakhov <malakhv@gmail.com>
2023-11-26 01:35:14 +0300

    Refactoring comments

--- app/src/ProgMsg.pas
+++ app/src/ProgMsg.pas
@@ -1,38 +1,38 @@
-{-------------------------------------------------------------------------}
-{                                                                         }
-{                          SACD-Ripper project                            }
-{                                                                         }
-{  Copyright (C) 1996-2022 Mikhail Malakhov <malakhv@gmail.com>           }
-{                                                                         }
-{  Licensed under the Apache License, Version 2.0 (the "License").        }
-{  You may not use this file except in compliance with the License.       }
-{  You may obtain a copy of the License at                                }
-{                                                                         }
-{     http://www.apache.org/licenses/LICENSE-2.0                          }
-{                                                                         }
-{  Unless required by applicable law or agreed to in writing, software    }
-{  distributed under the License is distributed on an "AS IS" BASIS,      }
-{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        }
-{  implied.                                                               }
-{                                                                         }
-{  See the License for the specific language governing permissions and    }
-{  limitations under the License.                                         }
-{                                                                         }
-{-------------------------------------------------------------------------}
+{------------------------------------------------------------------------------}
+{                                                                              }
+{                             SACD-Ripper project                              }
+{                                                                              }
+{  Copyright (C) 1996-2023 Mikhail Malakhov <malakhv@gmail.com>                }
+{                                                                              }
+{  Licensed under the Apache License, Version 2.0 (the "License"). You may     }
+{  not use this file except in compliance with the License. You may obtain     }
+{  a copy of the License at                                                    }
+{                                                                              }
+{     http://www.apache.org/licenses/LICENSE-2.0                               }
+{                                                                              }
+{  Unless required by applicable law or agreed to in writing, software         }
+{  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   }
+{  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            }
+{                                                                              }
+{  See the License for the specific language governing permissions and         }
+{  limitations under the License.                                              }
+{                                                                              }
+{------------------------------------------------------------------------------}
 
-{-------------------------------------------------------------------------}
-{ The Unit contains program messages.                                     }
-{                                                                         }
-{ Created: 11.11.2023                                                     }
-{ Author: Mikhail.Malakhov                                                }
-{-------------------------------------------------------------------------}
+{------------------------------------------------------------------------------}
+{ The Unit contains program messages.                                          }
+{                                                                              }
+{ Project: SACD-Ripper                                                         }
+{ Created: 11.11.2023                                                          }
+{ Author: Mikhail.Malakhov                                                     }
+{------------------------------------------------------------------------------}
 
-unit ProgMsg;
+unit ProgMsg;                                                           { UNIT }
 
 {$mode delphi}
 {$h+}
 
-interface
+interface                                                          { INTERFACE }
 
 { Program messages }
 const
@@ -51,7 +51,7 @@ const
 }
 procedure PrintHelp(ProgName: String);
 
-implementation
+implementation                                                { IMPLEMENTETION }
 
 const
     DEF_INDENT = '  ';
@@ -67,8 +67,6 @@ begin
         '- Display this information.');
 end;
 
-end.
+end.                                                                     { END }
 
-{-------------------------------------------------------------------------}
-{ END                                                                     }
-{-------------------------------------------------------------------------}
+{------------------------------------------------------------------------------}
